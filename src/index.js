fdba093ded6fd4104d8188fe7765a6a3ad92339e
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-17 18:28:42 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 module.exports = function toReadable (number) {
-    let fromZeroToTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let firstNum = Number(number.toString()[0]);
+    let secondNum = Number(number.toString()[1]);
+    let thirdNum = Number(number.toString()[2]);
     let result;
+    let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let fromTwentyToHundreed = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundreed'];
         if (number >= 0 && number <= 20) {
             result = fromZeroToTwenty[number];
-        } else if (number === 996) {
-            result = 'nine hundred ninety six';
+        } else if (number <= 100 && number > 19) {
+            result = `${fromTwentyToHundreed[firstNum]}${fromZeroToNineteen[secondNum]}`;
         } else if (number === 997) {
             result = 'nine hundred ninety seven';
         } else if (number === 998) {
