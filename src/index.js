fa5b0a8192adb8ad494899b41fb18d2b25632f47
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-17 21:19:37 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function toReadable (number) {
-    let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let fromZeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let fromTwentyToNinety = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let hundred = 'hundred';
     let firstNum = Number(number.toString()[0]);
@@ -7,11 +8,16 @@ module.exports = function toReadable (number) {
     let thirdNum = Number(number.toString()[2]);
     let fourthNum = Number(number.toString()[3]);
     let result;
-        if (number >= 0 && number <= 19) {
-            result = fromZeroToNineteen[number];
+        if (number >= 0 && number <= 9) {
+            result = fromZeroToNine[number];
+        } else if (number >= 10 && number <= 19) {
+            result = fromTenToNineteen[secondNum];
         } else if (number > 19 && number < 100) {
             result = `${fromTwentyToNinety[firstNum]} ${fromZeroToNineteen[secondNum]}`;
-        } else if (number >= 100 && number < 1000) {
-            result = `${fromZeroToNineteen[firstNum]} ${hundred} ${fromTwentyToNinety[thirdNum]} ${fromZeroToNineteen[fourthNum]}`};
+        } else if (number >= 100 && number <= 999 && thirdNum === 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[thirdNum]}`;
+        } else if (number >= 100 && number <= 999 && thirdNum !== 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[thirdNum]} ${fromZeroToNine[thirdNum]}`;
+        };
     return result
     };
