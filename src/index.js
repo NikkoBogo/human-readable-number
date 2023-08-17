ec57bc661b434fe75efb8aee3962dab82ab401f6
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-17 16:39:48 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
-  
-}
+    let fromZeroToTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let result;
+        if (number >= 0 && number <= 20) {
+            result = fromZeroToTwenty[number];
+        } else if (number === 996) {
+            result = 'nine hundred ninety six';
+        } else if (number === 997) {
+            result = 'nine hundred ninety seven';
+        } else if (number === 998) {
+            result = 'nine hundred ninety eight';
+        } else if (number === 999) {
+            result = 'nine hundred ninety nine';
+        }
+    return result
+    };
