d2305568a5f317081c836d272cec3c3005cbfe79
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-17 18:40:10 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function toReadable (number) {
+    let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let fromTwentyToHundreed = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundreed'];
     let firstNum = Number(number.toString()[0]);
     let secondNum = Number(number.toString()[1]);
     let thirdNum = Number(number.toString()[2]);
     let result;
-    let fromZeroToNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let fromTwentyToHundreed = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundreed'];
-        if (number >= 0 && number <= 20) {
+        if (number >= 0 && number <= 19) {
             result = fromZeroToTwenty[number];
-        } else if (number <= 100 && number > 19) {
-            result = `${fromTwentyToHundreed[firstNum]}${fromZeroToNineteen[secondNum]}`;
+        } else if (number > 19 && number <= 100) {
+            result = `${fromTwentyToHundreed[firstNum]} ${fromZeroToNineteen[secondNum]}`;
         } else if (number === 997) {
             result = 'nine hundred ninety seven';
         } else if (number === 998) {
