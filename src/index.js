8c5e3fc07870423c48f7d5ceeb0025ea7b812add
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-17 22:00:33 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable (number) {
     let fromZeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let fromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let fromTwentyToNinety = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let hundred = 'hundred';
     let firstNum = Number(number.toString()[0]);
@@ -16,16 +16,16 @@ module.exports = function toReadable (number) {
             result = `${fromTwentyToNinety[firstNum]} ${fromZeroToNine[secondNum]}`;
         } else if (number > 19 && number < 100 && secondNum === 0) {
             result = fromTwentyToNinety[firstNum];
-        } else if (number >= 120 && number <= 999 && secondNum === 0 && thirdNum === 0) {
-            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[thirdNum]}`;
-        } else if (number >= 120 && number <= 999 && secondNum === 0 && thirdNum !== 0) {
-            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromZeroToNine[thirdNum]}`;
-        } else if (number >= 120 && number <= 999 && secondNum !== 0 && thirdNum === 0) {
-            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[secondNum]}`;
-        } else if (number >= 120 && number <= 999 && secondNum !== 0 && thirdNum !== 0) {
-            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[secondNum]} ${fromZeroToNine[thirdNum]}`;
         } else if (number > 100 && number <= 119 && secondNum !== 0) {
             result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTenToNineteen[thirdNum]}`;
+        } else if (number >= 100 && number <= 999 && secondNum === 0 && thirdNum === 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[thirdNum]}`;
+        } else if (number >= 100 && number <= 999 && secondNum === 0 && thirdNum !== 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromZeroToNine[thirdNum]}`;
+        } else if (number >= 100 && number <= 999 && secondNum !== 0 && thirdNum === 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[secondNum]}`;
+        } else if (number >= 100 && number <= 999 && secondNum !== 0 && thirdNum !== 0) {
+            result = `${fromZeroToNine[firstNum]} ${hundred} ${fromTwentyToNinety[secondNum]} ${fromZeroToNine[thirdNum]}`;
         }
     return result
     };
